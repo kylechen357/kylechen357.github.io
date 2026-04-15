a074d31f43863d92604a8774f011a3e15dcf38a0
kyle <kylechen357@gmail.com>
2026-04-15 18:45:05 +0800

    main

--- google-apps-script-config.js
+++
@@ -1,8 +0,0 @@
-window.GOOGLE_SCRIPT_CONFIG = {
-  webAppUrl: "https://script.google.com/macros/s/AKfycbypa3gMumxGhYjBqoGnR-SvYvbG9mXYE8JoZ2Q_yDMx_BJbAlBUW9nheVAAMo2jin6w/exec"
-};
-
-window.isGoogleScriptConfigured = function isGoogleScriptConfigured() {
-  const config = window.GOOGLE_SCRIPT_CONFIG || {};
-  return Boolean(config.webAppUrl && !config.webAppUrl.includes("YOUR_DEPLOYMENT_ID"));
-};
